9b84cb303cb16b9358277768fd9f98a2c10f1543
Jakub Dębosz <56394099+Doseeey@users.noreply.github.com>
2025-11-05 20:17:27 +0100

    Added history fetch, noise, handling times

--- sensor/Sensor.py
+++ sensor/Sensor.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 import thingspeak
 import datetime
+import random
 
 from azure.iot.device.aio import IoTHubDeviceClient
 from azure.iot.device import Message
@@ -12,9 +13,32 @@ class Sensor:
         self.deviceId = deviceId
         self.sharedAccessKey = sharedAccessKey
 
-        self.history = [1, 2, 3, 4, 5] #to do for c2d
+    def _get_data_from_thingspeak(self, n):
+        #Main channel 2938875
+        #Backup channel 819972
+        ch = thingspeak.Channel(id=819972)
+        data = json.loads(ch.get({'results': n}))
 
-        
+        messages = []
+
+        for i in range(n):
+            feed = data["feeds"][i]
+
+            #can be used when will work
+            #time = datetime.datetime.fromisoformat(feed["created_at"].replace("Z", "+00:00")).strftime("%Y-%m-%d %H:%M:%S.%f")
+
+            # Added fake noise
+            temperature = float(feed["field1"]) + random.uniform(-0.01, 0.01)
+            humidity = float(feed["field2"]) + random.uniform(-0.01, 0.01)
+
+            # temperature = float(feed["field1"])
+            # humidity = float(feed["field2"])
+
+            message = Message(f'{{"time": "{datetime.datetime.now()-datetime.timedelta(seconds=6*i)}", "temperature": "{round(temperature, 2)}", "humidity": "{round(humidity, 2)}"}}')
+            messages.append(message)
+
+        return messages
+
     async def connect(self):
         """Connect to IoT Hub"""
         CONNECTION_STRING = f"HostName={self.hostname};DeviceId={self.deviceId};SharedAccessKey={self.sharedAccessKey}"
@@ -24,14 +48,8 @@ class Sensor:
     async def _send_sensor_data_to_iot_hub(self):
         """Send telemetry to IoT Hub"""
         while True:
-            ch = thingspeak.Channel(id=2938875)
-            data = json.loads(ch.get({'results': 1}))
-            feed = data["feeds"][0]
-
-            temperature = feed["field1"]
-            humidity = feed["field2"]
-
-            message = Message(f'{{"time": "{datetime.datetime.now()}", "temperature": "{temperature}", "humidity": "{humidity}"}}')
+            messages = self._get_data_from_thingspeak(1)
+            message = messages[0]
             await self.client.send_message(message)
 
             print(f"Sent message: {message}")
@@ -49,10 +67,12 @@ class Sensor:
                 if cmd.get("command") == "get_history":
                     # Send the last N readings
                     n = cmd.get("count", 5)
-                    subset = self.history[-n:]
-                    reply = Message(json.dumps({"history": subset}))
-                    await self.client.send_message(reply)
-                    print(f"Sent {len(subset)} historical readings back to IoT Hub.")
+                    messages = self._get_data_from_thingspeak(n)
+                    for message in messages:
+                        await self.client.send_message(message)
+                        print(f"Sent message from C2D: {message}")
+
+                    print(f"Sent historical readings back to IoT Hub.")
             except Exception as e:
                 print("Error handling command:", e)
 
--- server/MainWindow.py
+++ server/MainWindow.py
@@ -1,16 +1,18 @@
 import asyncio
-import aiohttp
 import json
+import math
+import matplotlib
+
+import matplotlib.pyplot as plt
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import QTimer
 from azure.eventhub.aio import EventHubConsumerClient
 from azure.iot.hub import IoTHubRegistryManager
-import matplotlib
-matplotlib.use("Qt5Agg")
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from datetime import datetime, timedelta
 
+matplotlib.use("Qt5Agg")
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, hostname: str, deviceId: str, sharedAccessKey: str, sharedAccessKeyName: str, eventHubHostName: str, eventHubEntityPath: str):
@@ -18,15 +20,23 @@ class MainWindow(QtWidgets.QMainWindow):
 
         uic.loadUi("iotgui.ui", self)
 
-        self.CONSUMER_GROUP = "$Default"
+        # IoT Hub Data
+        self.CONSUMER_GROUP = "testgroup"
         self.DEVICE_ID = deviceId
         self.EVENTHUB_CONNECTION_STR = f"Endpoint={eventHubHostName};SharedAccessKeyName={sharedAccessKeyName};SharedAccessKey={sharedAccessKey};EntityPath={eventHubEntityPath}"
         self.SERVICE_CONNECTION_STR = f"HostName={hostname};SharedAccessKeyName={sharedAccessKeyName};SharedAccessKey={sharedAccessKey}"
 
+        # Data arrays
         self.timeData = []
         self.temperatureData = []
         self.humidityData = []
 
+        # Alerts
+        self.dew_alert = True
+        self.temp_alert = True
+        self.humidity_alert = True
+
+        # Plots
         self.temp_fig = Figure(figsize=(4,3), facecolor='#F0F0F0')
         self.temp_canvas = FigureCanvas(self.temp_fig)
         self.temp_plot_layout = QtWidgets.QVBoxLayout(self.temp_plot)
@@ -37,25 +47,38 @@ class MainWindow(QtWidgets.QMainWindow):
         self.hum_plot_layout = QtWidgets.QVBoxLayout(self.hum_plot)
         self.hum_plot_layout.addWidget(self.hum_canvas)
 
-
+        # Timers
         self.plot_timer = QTimer(self)
         self.plot_timer.timeout.connect(self.update_interface)
         self.plot_timer.start(1000)
 
-        # # QTimer to trigger periodic updates
-        # self.timer = QTimer(self)
-        # # self.timer.timeout.connect(self.update_data)
-        # self.timer.singleShot(1000, self.update_data)  # 5 seconds
-
         # Buttons
         self.startButton.clicked.connect(self.update_data)
+        self.fetch_history_button.clicked.connect(self.get_history)
+
+    def logAction(self, action):
+        text: str = self.log_window.toPlainText()
+        newText: str = text + f"[{datetime.now().strftime('%H:%M:%S')}] {action}\n"
+        text: str = self.log_window.setPlainText(newText)
+        self.log_window.verticalScrollBar().setValue(self.log_window.verticalScrollBar().maximum())
 
     async def on_event(self, partition_context, event):
         print(f"[Telemetry] {event.body_as_str()}")
         telemetry = json.loads(event.body_as_str())
-        self.timeData.append(datetime.fromisoformat(telemetry["time"]))
-        self.temperatureData.append(telemetry["temperature"])
-        self.humidityData.append(telemetry["humidity"])
+
+        if len(self.timeData) < 2:
+            self.timeData.append(datetime.fromisoformat(telemetry["time"]))
+            self.temperatureData.append(float(telemetry["temperature"]))
+            self.humidityData.append(float(telemetry["humidity"]))
+        elif (datetime.fromisoformat(telemetry["time"]) < self.timeData[0] or datetime.fromisoformat(telemetry["time"]) > self.timeData[-1]):
+            self.timeData.append(datetime.fromisoformat(telemetry["time"]))
+            self.temperatureData.append(float(telemetry["temperature"]))
+            self.humidityData.append(float(telemetry["humidity"]))
+
+        #Sort to assert assert proper times
+        combined = list(zip(self.timeData, self.temperatureData, self.humidityData))
+        combined.sort(key=lambda x: x[0])
+        self.timeData, self.temperatureData, self.humidityData = map(list, zip(*combined))
 
     async def receive_telemetry(self):
         client = EventHubConsumerClient.from_connection_string(
@@ -63,25 +86,95 @@ class MainWindow(QtWidgets.QMainWindow):
             consumer_group=self.CONSUMER_GROUP
             )
         async with client:
-            print(" Listening for telemetry from IoT Hub...")
+            self.logAction("Connected to IoT Hub")
+            self.logAction("Listening for telemetry from IoT Hub...")
             await client.receive(
                 on_event=self.on_event,
-                starting_position="-1", # from beginning
+                starting_position="@latest",
                 )
     
     def update_data(self):
-        # Schedule async task without blocking GUI
         asyncio.create_task(self.receive_telemetry())
 
-    def update_interface(self):
-        self.update_plot()
-        if len(self.temperatureData): 
-            latest_temp = self.temperatureData[-1]
-            self.temperature_label.setText(f"{latest_temp} °C")
-        if len(self.humidityData): 
-            latest_humidity = self.humidityData[-1]
-            self.humidity_label.setText(f"{latest_humidity} %")
+    def get_history(self):
+        elems = self.historyNumber.text()
+
+        if not elems.isdigit():
+            self.logAction("Invalid number entered for history. Must be an integer.")
+            return
         
+        self.logAction(f"Request {elems} history data elements from Sensor")
+
+        manager = IoTHubRegistryManager(self.SERVICE_CONNECTION_STR)
+        payload = json.dumps({"command": "get_history", "count": int(elems)})
+        manager.send_c2d_message(self.DEVICE_ID, payload)
+
+    def calculate_dew_point(self):
+        if len(self.temperatureData) and len(self.humidityData):
+            a = 17.27
+            b = 237.7
+            temp = float(self.temperatureData[-1])
+            hum = float(self.humidityData[-1])
+            alpha = ((a * temp) / (b + temp)) + math.log(float(hum) / 100.0)
+            return (b * alpha) / (a - alpha)
+        return 0 
+    
+    def update_dew_point(self):
+        dew_point = self.calculate_dew_point()
+        self.dew_label.setText(f"{dew_point:.2f} °C")
+
+        if dew_point >= 15 and dew_point <= 18:
+            self.dew_label.setStyleSheet("color: #fcba03")
+            if self.dew_alert:
+                self.logAction("Triggered minor alert for Dew Point")
+                self.dew_alert = False
+        elif dew_point > 18:
+            self.dew_label.setStyleSheet("color: red")
+            if self.dew_alert:
+                self.logAction("Triggered major alert for Dew Point")
+                self.dew_alert = False
+        else:
+            self.dew_label.setStyleSheet("color: green")
+            self.dew_alert = True
+
+    def update_temperature(self):
+        if len(self.temperatureData): 
+            latest_temp = float(self.temperatureData[-1])
+            self.temperature_label.setText(f"{latest_temp:.2f} °C")
+
+            if latest_temp >= 25 and latest_temp <= 27:
+                self.temperature_label.setStyleSheet("color: #fcba03")
+                if self.temp_alert:
+                    self.logAction("Triggered minor alert for Temperature")
+                    self.temp_alert = False
+            elif latest_temp > 27:
+                self.temperature_label.setStyleSheet("color: red")
+                if self.temp_alert:
+                    self.logAction("Triggered major alert for Temperature")
+                    self.temp_alert = False
+            else:
+                self.temperature_label.setStyleSheet("color: green")
+                self.temp_alert = True
+
+    def update_humidity(self):
+        if len(self.humidityData): 
+            latest_humidity = float(self.humidityData[-1])
+            self.humidity_label.setText(f"{latest_humidity:.2f} %")
+
+            if latest_humidity >= 60 and latest_humidity <= 70:
+                self.humidity_label.setStyleSheet("color: #fcba03")
+                if self.humidity_alert:
+                    self.logAction("Triggered minor alert for Humidity")
+                    self.humidity_alert = False
+            elif latest_humidity > 70:
+                self.humidity_label.setStyleSheet("color: red")
+                if self.humidity_alert:
+                    self.logAction("Triggered major alert for Humidity")
+                    self.humidity_alert = False
+            else:
+                self.humidity_label.setStyleSheet("color: green")
+                self.humidity_alert = True
+    
     def update_plot(self):
         if not self.timeData:
             return
@@ -90,11 +183,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.temp_fig.clear()
         ax1 = self.temp_fig.add_subplot(111)
         ax1.plot(self.timeData, self.temperatureData)
-        ax1.set_xlim(self.timeData[-20], self.timeData[-1])
+        ax1.set_xlim(datetime.now()-timedelta(minutes=5), datetime.now())
         ax1.set_title("Temperature")
         ax1.set_xlabel("Time")
         ax1.set_ylabel("Temperature [°C]")
         ax1.set_facecolor("#F0F0F0")
+        ax1.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%H:%M'))
         ax1.grid(True, linestyle='-', linewidth=0.5)
         self.temp_canvas.draw()
 
@@ -102,11 +196,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.hum_fig.clear()
         ax2 = self.hum_fig.add_subplot(111)
         ax2.plot(self.timeData, self.humidityData)
-        ax2.set_xlim(self.timeData[-20], self.timeData[-1])
+        ax2.set_xlim(datetime.now()-timedelta(minutes=5), datetime.now())
         ax2.set_title("Humidity")
         ax2.set_xlabel("Time")
         ax2.set_ylabel("Humidity [%]")
         ax2.set_facecolor("#F0F0F0")
+        ax2.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%H:%M'))
         ax2.grid(True, linestyle='-', linewidth=0.5)
         self.hum_canvas.draw()
 
+    def update_interface(self):
+        self.update_plot()
+        self.update_temperature()
+        self.update_humidity()
+        self.update_dew_point()
